f0ff595e8d2693c7e48f5769d66fe54f76c61f5d
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-09-11 18:11:36 +0200

    add prisma module and export prisma service. Next prisma service import to users module and export user service

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 import * as cors from 'cors';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
