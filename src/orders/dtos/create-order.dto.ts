59d76a8ffdcfa7cdbd6791adae4bdb6633be2144
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-09-19 13:28:26 +0200

    add class-validator to create orders dto

--- src/orders/dtos/create-order.dto.ts
+++ src/orders/dtos/create-order.dto.ts
@@ -11,18 +11,61 @@
 //   client User @relation(fields: [userId], references: [id])
 //   userId String
 
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
 //   order OrderDetails[]
 
 export class CreateOrderDTO {
+  @IsNotEmpty()
+  @IsString()
   firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
   lastName: string;
+
+  @IsNotEmpty()
+  @IsString()
   address: string;
+
+  @IsNotEmpty()
+  @IsString()
   city: string;
+
+  @IsNotEmpty()
+  @IsString()
   phoneNumber: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   deliveryType: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   amountPay: number;
+
+  @IsNotEmpty()
+  @IsNumber()
   amountProducts: number;
+
+  @IsArray()
+  @ArrayNotEmpty()
   order: Array<string>;
+
+  @IsString()
+  @IsUUID()
+  @IsNotEmpty()
   userId: string;
 }
